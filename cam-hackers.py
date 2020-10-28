b68acc149f9c11f3657e5c6b946e073dda04ec90
Dorian Ben Haim <dorianbenhaim7@gmail.com>
2020-10-28 23:41:52 +0200

    Country Code Array bug (wrong indexes) was fixed Also added option to choose cameras by cities and improve printing

--- cam-hackers.py
+++ cam-hackers.py
@@ -1,8 +1,12 @@
-#!/usr/bin/env python3
-#-*- coding: utf-8 -*-
-#github.com/AngelSecurityTeam/Cam-Hackers
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# github.com/AngelSecurityTeam/Cam-Hackers
 
-import requests, re , colorama
+import colorama
+import re
+import requests
+import html
+
 colorama.init()
 print("""
 \033[1;31m\033[1;37m ██████╗ █████╗ ███╗   ███╗      ██╗  ██╗ █████╗  ██████╗██╗  ██╗███████╗██████╗ ███████╗
@@ -44,26 +48,60 @@ print("""
 \033[1;31m30) \033[1;37mArgentina                   \033[1;31m60) \033[1;37mPanama                \033[1;31m90) \033[1;37mUruguay
                                                           \033[1;31m91) \033[1;37mExtra
 """)
+data_to_extract = []
+
+
+def extract_data(data):
+    extracted_data = []
+    for camera in data:
+        ip = camera.split("src=\"")[1].split("\"")[0]
+        camera_type = camera.split("title=\"View ")[1].split(" CCTV")[0]
+        city = camera.split("title=\"")[1].split(", ")[1].split("\"")[0]
+        extracted_data.append({'ip': ip, 'type': camera_type, 'city': city})
+    return extracted_data
+
+
+def unique(obj):
+    unique_city = []
+
+    for camera in obj:
+        if camera['city'] not in unique_city:
+            unique_city.append(camera['city'])
+    return unique_city
+
+
+def print_cameras(cts, cams, index):
+
+    if len(cts) != index:
+        cam_to_show = list(filter(lambda x: x['city'] == cts[index], cams))
+    else:
+        cam_to_show = list(cams)
+    for i, c in enumerate(cam_to_show):
+        txt = """\033[1;31m""" + str(i) + """) \033[1;37m""" + """\033[1;31mUrl:\033[1;37m """ + c['ip'] + \
+              """ | \033[1;31mType:\033[1;37m """ + c['type'] + \
+              """ | \033[1;31mCity:\033[1;37m """ + c['city']
+        print(txt)
+
 
 try:
     print()
     countries = ["US", "JP", "IT", "KR", "FR", "DE", "TW", "RU", "GB", "NL",
-                 "CZ", "TR", "AT", "CH", "ES", "CA", "SE", "IL", "PL", "IR",
-                 "NO", "RO", "IN", "VN", "BE", "BR", "BG", "ID", "DK", "AR",
+                 "CZ", "TR", "AT", "CH", "ES", "CA", "SE", "IL", "IR", "PL",
+                 "IN", "NO", "RO", "VN", "BE", "BR", "BG", "ID", "DK", "AR",
                  "MX", "FI", "CN", "CL", "ZA", "SK", "HU", "IE", "EG", "TH",
                  "UA", "RS", "HK", "GR", "PT", "LV", "SG", "IS", "MY", "CO",
                  "TN", "EE", "DO", "SI", "EC", "LT", "PS", "NZ", "BD", "PA",
-                 "MD", "NI", "MT", "IT", "SA", "HR", "CY", "PK", "AE", "KZ",
+                 "MD", "NI", "MT", "TT", "SA", "HR", "CY", "PK", "AE", "KZ",
                  "KW", "VE", "GE", "ME", "SV", "LU", "CW", "PR", "CR", "BY",
                  "AL", "LI", "BA", "PY", "PH", "FO", "GT", "NP", "PE", "UY",
                  "-"]
     headers = {"User-Agent": "Mozilla/5.0 (X11; Linux i686; rv:68.0) Gecko/20100101 Firefox/68.0"}
 
     num = int(input("OPTIONS : "))
-    if num not in range(1, 91+1):
+    if num not in range(1, 91 + 1):
         raise IndexError
 
-    country = countries[num-1]
+    country = countries[num - 1]
     res = requests.get(
         f"https://www.insecam.org/en/bycountry/{country}", headers=headers
     )
@@ -74,11 +112,51 @@ try:
             f"https://www.insecam.org/en/bycountry/{country}/?page={page}",
             headers=headers
         )
-        find_ip = re.findall(r"http://\d+.\d+.\d+.\d+:\d+", res.text)
-        for ip in find_ip:
-            print("\033[1;31m", ip)
-except:
-    pass
+        # find_ip = re.findall(r"http://\d+.\d+.\d+.\d+:\d+", res.text)^.*token.*$
+        relevant_lines = re.findall(r"^.*http://\d+.\d+.\d+.\d+:\d+.*$", res.text, re.MULTILINE)
+
+        for line in relevant_lines:
+            data_to_extract.append(line)
+            # print("\033[1;31m", ip)
+    cameras = extract_data(data_to_extract)
+    print()
+    print('\033[1;31mTotal found cameras: ', str(len(cameras)))
+    print('\033[1;31mChoose city:')
+    cities = unique(cameras)
+    cities = sorted(cities)
+    cities_to_print = ""
+    count = 0
+    for idx, ct in enumerate(cities):
+        max_chars = 29
+
+        if "&#" in ct:
+            cities[idx] = html.unescape(ct)
+            ct = html.unescape(ct)
+
+        if count == 3:
+            cities_to_print += '\n'
+            count = 0
+
+        if idx >= 10 or (idx >= 10 and idx == 0):
+            max_chars -= 1
+
+        if len(ct) < max_chars:
+            spaces = max_chars - len(ct)
+
+        cities_to_print += """\033[1;31m""" + str(idx) + """) \033[1;37m""" + ct + (" " * spaces)
+        if idx == len(cities) - 1:
+            print(cities_to_print + """\033[1;31m""" + str(len(cities)) + """) \033[1;37m""" + "All Cameras")
+
+        count += 1
+
+    num = int(input("OPTIONS : "))
+    if num not in range(0, len(cities)+1):
+        raise IndexError
+    print()
+    print_cameras(cities, cameras, num)
+
+except Exception as e:
+    print(e)
 finally:
     print("\033[1;37m")
     exit()
